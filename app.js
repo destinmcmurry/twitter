d34e654f7429b635b25533905e89ea617f431572
Tasnuva Noor <noortasnuva@gmail.com>
2018-03-06 14:13:37 -0500

    initiated homepage

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
+const routes = require('./routes')
 
 
+app.use('/', routes);
 app.use(function (req, res, next) {
     console.log(`${req.method} ${req.path}`);
     next();
 });
+app.use(express.static('public'))
 
 app.set('view engine', 'html'); 
 app.engine('html', nunjucks.render); 
 nunjucks.configure('views', {noCache: true});
 
-app.get('/', (req, res) => {
-    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-    res.render('index', {title: 'Hall of Fame', people: people});
-});
+
+
+// app.get('/', (req, res) => {
+//     const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//     res.render('index', {title: 'Hall of Fame', people: people});
+// });
 
 // const locals  = {
 //     title: 'An Example',
