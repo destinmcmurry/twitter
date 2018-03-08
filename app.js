9f22cc58fa5b2f6e6855e4e314dbdba85a127774
Destin McMurry <destinmcmurry@gmail.com>
2018-03-07 22:34:27 -0500

    functional tweetable homepage!

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
-const routes = require('./routes')
-
-
-app.use('/', routes);
-app.use(function (req, res, next) {
-    console.log(`${req.method} ${req.path}`);
-    next();
-});
-app.use(express.static('public'))
+const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'html'); 
 app.engine('html', nunjucks.render); 
 nunjucks.configure('views', {noCache: true});
 
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', routes);
+
+// app.use(function (req, res, next) {
+//     console.log(`${req.method} ${req.path}`);
+//     next();
+// });
 
 // app.get('/', (req, res) => {
 //     const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
@@ -34,7 +37,6 @@ nunjucks.configure('views', {noCache: true});
 // nunjucks.render('index.html', locals, function(err, output) {
 //     console.log(output);
 // });
-
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,20 @@ const tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   let allTweets = tweetBank.list();
-  res.render( 'index', { tweets: allTweets } );
+  res.render( 'index', { tweets: allTweets, showForm: true } );
 });
 
-// change something
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var tweetsForName = tweetBank.find( {name: name} );
   res.render( 'index', { tweets: tweetsForName } );
 });
 
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
 
 module.exports = router;
